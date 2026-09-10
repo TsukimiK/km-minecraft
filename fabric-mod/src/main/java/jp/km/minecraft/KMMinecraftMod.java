2b7e39001327073403884cbb4353381a249977df
TsukimiK <kkcom0205@gmail.com>
2026-09-10 20:53:29 +0900

    Add Fabric Java entrypoint

---
+++ fabric-mod/src/main/java/jp/km/minecraft/KMMinecraftMod.java
@@ -0,0 +1,30 @@
+package jp.km.minecraft;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+
+/**
+ * KM Minecraft Fabric server mod bootstrap.
+ *
+ * <p>The old external datapack is not required. Gameplay data is bundled in the mod JAR,
+ * while Java owns initialization, command registration and tick dispatch.</p>
+ */
+public final class KMMinecraftMod implements ModInitializer {
+    public static final String MOD_ID = "km-minecraft";
+    public static final String VERSION = "2.0.0";
+
+    @Override
+    public void onInitialize() {
+        System.out.println("[KM Minecraft] Fabric entrypoint active - v" + VERSION);
+
+        KMCommands.register();
+
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            System.out.println("[KM Minecraft] Server started; initializing KM runtime.");
+            KMRuntime.onServerStarted(server);
+        });
+
+        ServerTickEvents.END_SERVER_TICK.register(KMRuntime::onServerTick);
+    }
+}
